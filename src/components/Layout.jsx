ea774949640caf2efd8e8ffdfe1e1a3e4fe5bc17
Nahum Casco <nahumcasco.v@gmail.com>
2022-09-05 22:23:41 -0500

    footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,12 @@ export const Layout = ({ children }) => {
     return (
         <main>
             <Navbar />
-            <div className="pt-[80px] bg-plt-cream">
+            <div className="pt-[80px] md:pt-[60px] bg-white">
                 {children}
             </div>
+            <footer className="w-full h-80 bg-plt-blue">
+
+            </footer>
         </main>
     )
 }
