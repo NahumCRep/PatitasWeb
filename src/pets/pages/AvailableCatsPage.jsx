ab69f017e2a17859827a80b2274fc1f42e1f7384
Nahum Casco <nahumcasco.v@gmail.com>
2022-09-09 22:26:03 -0500

    available pets page initial design

---
+++ src/pets/pages/AvailableCatsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AvailablePetsLayout } from '../../components/layouts'
+
+export const AvailableCatsPage = () => {
+  return (
+    <AvailablePetsLayout pet={'gatos'}>
+
+    </AvailablePetsLayout>
+  )
+}
---
+++ src/pets/pages/AvailableDogsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AvailablePetsLayout } from '../../components/layouts'
+
+export const AvailableDogsPage = () => {
+  return (
+    <AvailablePetsLayout pet={'perros'}>
+
+    </AvailablePetsLayout>
+  )
+}
