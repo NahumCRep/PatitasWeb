31be4d40b0859312d79628a3763a9652a8b5e185
Nahum Casco <nahumcasco.v@gmail.com>
2022-09-11 00:31:47 -0500

    get route params

--- src/pets/pages/AvailableCatsPage.jsx
+++ src/pets/pages/AvailableCatsPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { AvailablePetsLayout } from '../../components/layouts'
+import { useParams } from 'react-router-dom';
+// ** Icons
+import { FaCat } from 'react-icons/fa';
 
 export const AvailableCatsPage = () => {
+  const {province, page} = useParams();
   return (
-    <AvailablePetsLayout pet={'gatos'}>
-
+    <AvailablePetsLayout pet={'cat'}>
+      <div className="w-full h-16 px-5 flex items-center gap-4">
+        <div className="w-11 aspect-square text-white flex items-center justify-center bg-plt-cream">
+          <FaCat size={35} /> 
+        </div>
+        <p className="font-secondary font-semibold text-slate-400">gatos { province && `/  ${province.replaceAll('-', ' ')}`}</p>
+      </div>
+      <div className="grid-cols-2 md:grid-cols-4">
+       
+      </div>
     </AvailablePetsLayout>
   )
 }
--- src/pets/pages/AvailableDogsPage.jsx
+++ src/pets/pages/AvailableDogsPage.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { AvailablePetsLayout } from '../../components/layouts'
+import { useParams } from 'react-router-dom';
+// ** Icons
+import { FaDog } from 'react-icons/fa';
 
 export const AvailableDogsPage = () => {
-  return (
-    <AvailablePetsLayout pet={'perros'}>
+  const {province, page} = useParams();
 
+  // const showParams = useMemo(() => {
+  //   console.log('llamada');
+  // }, [params])
+
+  useEffect(() => {
+    console.log({province, page});
+  }, [province, page])
+
+  return (
+    <AvailablePetsLayout pet={'dog'}>
+      <div className="w-full h-16 px-5 flex items-center gap-4">
+        <div className="w-11 aspect-square text-white flex items-center justify-center bg-plt-blue">
+          <FaDog size={35} /> 
+        </div>
+        <p className="font-secondary font-semibold text-slate-400">perros { province && `/  ${province.replaceAll('-', ' ')}`}</p>
+      </div>
+      <div className="grid-cols-2 md:grid-cols-4">
+       
+      </div>
     </AvailablePetsLayout>
   )
 }
