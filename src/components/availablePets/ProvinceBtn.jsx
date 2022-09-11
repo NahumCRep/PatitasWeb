7a586106d82c4736f61d107ebcf0f66073dd9f35
Nahum Casco <nahumcasco.v@gmail.com>
2022-09-11 00:29:08 -0500

    responsive province menu

---
+++ src/components/availablePets/ProvinceBtn.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const ProvinceBtn = ({province, basePath}) => {
+    return (
+        <li className="w-full h-10">
+            <Link
+                to={`/${basePath}/${province.value}`}
+                className="flex items-center w-full h-full px-7 md:px-3 transition-colors  md:bg-white md:hover:bg-slate-200"
+            >
+                {province.province}
+            </Link>
+        </li>
+    )
+}
---
+++ src/localData/provinces.js
@@ -0,0 +1,38 @@
+export const provinces = [
+        {
+            "province":"Panamá",
+            "value":"panama"
+        },
+        {
+            "province":"Colón",
+            "value":"colon"
+        },
+        {
+            "province":"Darién",
+            "value":"darien"
+        },
+        {
+            "province":"Coclé",
+            "value":"cocle"
+        },
+        {
+            "province":"Veraguas",
+            "value":"veraguas"
+        },
+        {
+            "province":"Bocas del Toro",
+            "value":"bocas-del-toro"
+        },
+        {
+            "province":"Herrera",
+            "value":"herrera"
+        },
+        {
+            "province":"Los Santos",
+            "value":"los-santos"
+        },
+        {
+            "province":"Chiriquí",
+            "value":"chiriqui"
+        }
+]
