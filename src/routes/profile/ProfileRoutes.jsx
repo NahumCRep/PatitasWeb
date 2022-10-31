9f360a76f98ca8d451b7342752a5f3c1bf061015
Nahum Casco <nahumcasco.v@gmail.com>
2022-10-30 20:32:42 -0500

    set path urls

--- src/routes/profile/ProfileRoutes.jsx
+++ src/routes/profile/ProfileRoutes.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { NotFoundPage } from '../../pages/publicPages';
-import { ProfilePage } from '../../pages/profile';
+import { ProfilePage, ProfilePublicationsPage } from '../../pages/profile';
 
 export const ProfileRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<ProfilePage />} />
-            <Route path='/' element={<ProfilePage />} />
-            <Route path='/informacion' element={<ProfilePage />} />
+            <Route path='publicaciones' element={<ProfilePublicationsPage />} />
+            <Route path='informacion' element={<ProfilePage />}/>
             <Route path='*' element={<NotFoundPage />} />
         </Routes>
     )
