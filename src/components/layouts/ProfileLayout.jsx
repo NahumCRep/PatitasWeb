cbfc6fddbe00cfd53a79267097739a4a4cca1247
Nahum Casco <nahumcasco.v@gmail.com>
2022-11-07 21:52:57 -0500

    set layout title prop

--- src/components/layouts/ProfileLayout.jsx
+++ src/components/layouts/ProfileLayout.jsx
@@ -5,7 +5,7 @@ import { HiOutlineMenuAlt3, HiOutlineX } from 'react-icons/hi';
 // ** Images
 import { SideNav } from '../profile';
 
-export const ProfileLayout = ({ children }) => {
+export const ProfileLayout = ({ children, layoutTitle }) => {
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
 
   const handleNavOpen = () => {
@@ -31,7 +31,9 @@ export const ProfileLayout = ({ children }) => {
       <div className="flex-grow h-auto min-h-screen md:ml-[18%] ">
         <div className="w-full h-32 p-4 bg-svg-bg-hd bg-no-repeat bg-cover">
           <div className="w-full h-full p-4 font-secondary flex items-center justify-between">
-            <h1 className="text-3xl font-semibold md:text-6xl">Mi Panel</h1>
+            <h1 className="text-3xl font-semibold md:text-6xl">
+              {layoutTitle}
+            </h1>
             <button className=" hidden text-white bg-plt-blue p-2 rounded-xl border-b-8 border-plt-darkblue
                 transition-all duration-150 hover:border-b-2 hover:translate-y-1 md:inline">
               cerrar sesion
