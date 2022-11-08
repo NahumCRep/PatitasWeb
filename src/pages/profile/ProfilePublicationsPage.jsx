502e44ba3beca448607611899a95e2cca0fd2114
Nahum Casco <nahumcasco.v@gmail.com>
2022-11-07 21:55:14 -0500

    redirect to create page function handler

--- src/pages/profile/ProfilePublicationsPage.jsx
+++ src/pages/profile/ProfilePublicationsPage.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { ProfileLayout } from '../../components/layouts'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ProfileLayout } from '../../components/layouts';
 // icons
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 
 export const ProfilePublicationsPage = () => {
+  const navigate = useNavigate();
+
+  const redirectToCreatePublicationPage = () => {
+    navigate('crear');
+  }
+
   return (
-    <ProfileLayout>
+    <ProfileLayout layoutTitle={'Publicaciones'}>
         <div className="w-full h-9 flex items-center">
           <h2 className="font-secondary">Mis Publicaciones</h2>
         </div>
         <div className="mt-3">
-          <div className="flex items-center justify-center cursor-pointer md:w-52 md:h-52 bg-slate-100 text-slate-400 transition-colors duration-200  hover:text-plt-darkcream">
+          <button
+            onClick={() => redirectToCreatePublicationPage()} 
+            className="flex items-center justify-center cursor-pointer md:w-52 md:h-52 bg-slate-100 text-slate-400 transition-colors duration-200  hover:text-plt-darkcream">
             <BsFillPlusCircleFill size={60} />
-          </div>
+          </button>
         </div>
     </ProfileLayout>
   )
