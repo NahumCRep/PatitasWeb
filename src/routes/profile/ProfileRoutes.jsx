6df1e7fcb3644eaee922677d32b69a2ae4a8bd74
Nahum Casco <nahumcasco.v@gmail.com>
2022-11-07 21:54:33 -0500

    path to create publication page added

--- src/routes/profile/ProfileRoutes.jsx
+++ src/routes/profile/ProfileRoutes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { NotFoundPage } from '../../pages/publicPages';
-import { ProfilePage, ProfilePublicationsPage } from '../../pages/profile';
+import { ProfilePage, ProfilePublicationsPage, CreatePublicationPage } from '../../pages/profile';
 
 export const ProfileRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<ProfilePage />} />
             <Route path='publicaciones' element={<ProfilePublicationsPage />} />
+            <Route path='publicaciones/crear' element={<CreatePublicationPage />} />
             <Route path='informacion' element={<ProfilePage />}/>
             <Route path='*' element={<NotFoundPage />} />
         </Routes>
