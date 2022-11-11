e2827843704f0f4074f4cd789afdbf3834aef85b
Nahum Casco <nahumcasco.v@gmail.com>
2022-11-10 19:32:08 -0500

    wrapped pet state buttons within a div

--- src/components/profile/CreatePublicationForm.jsx
+++ src/components/profile/CreatePublicationForm.jsx
@@ -51,59 +51,54 @@ export const CreatePublicationForm = ({ handleSubmit }) => {
             }}
         >
             {({ errors, touched, isSubmitting }) => (
-                <>
-                <h2>Que tipo de mascota es?</h2>
-                 <button onClick={handlePetState} className={`w-[7rem] py-1 ${isDog ? 'bg-plt-blue text-white' : 'grayButton'}`}>
-                                    Perro
-                                </button>
-                                <button onClick={handlePetState} className={`w-[7rem] py-1 ${!isDog ? 'bg-plt-cream' : 'grayButton'} `}>
-                                    Gato
-                                </button>
-                <Form className="">
-                    <div className='w-full flex gap-5 mt-5'>
+                <div>
+                    <h2>Que tipo de mascota es?</h2>
+                    <div className="flex mt-2">
+                        <button onClick={handlePetState} className={`w-[7rem] py-1 ${isDog ? 'bg-plt-blue text-white' : 'grayButton'}`}>
+                            Perro
+                        </button>
+                        <button onClick={handlePetState} className={`w-[7rem] py-1 ${!isDog ? 'bg-plt-cream' : 'grayButton'} `}>
+                            Gato
+                        </button>
+                    </div>
+                    <Form>
+                        <div className='w-full flex flex-col gap-5 mt-5 md:flex-row'>
 
-                        <PetProfilePhoto pet={isDog} />
+                            <PetProfilePhoto pet={isDog} />
 
-                        <div className="w-full md:w-[60%] font-secondary">
-                            
-
-                            <div className="flex mt-2">
-                               
-                            </div>
-                            <div className="grid grid-cols-2 gap-5 mt-5">
-                                <SubForm touched={touched} errors={errors} />
+                            <div className="w-full md:w-[60%] font-secondary">
+                                <SubForm touched={touched} errors={errors} />   
                             </div>
                         </div>
-                    </div>
 
-                    <div className="mt-3">
-                        <label htmlFor='description'>Descripcion</label>
-                        <Field as="textarea" name="description" className="formFieldComponent h-28 resize-none" />
-                    </div>
-
-                    <div className="w-full mt-5">
-                        <div className='flex items-center gap-2'>
-                            Imagenes adicionales
-                            <label htmlFor='extraImgs' className='text-slate-400 bg-slate-200 w-7 h-7 flex items-center justify-center cursor-pointer transition-colors duration-200 hover:bg-slate-300'>
-                                <FaPlus size={15} />
-                            </label>
-                            <input type="file" id="extraImgs" className='hidden' />
+                        <div className="mt-3">
+                            <label htmlFor='description'>Descripcion</label>
+                            <Field as="textarea" name="description" className="formFieldComponent h-28 resize-none" />
                         </div>
-                        <hr className='mt-2' />
-                        <div className='mt-3'>
-                            <div className='w-40 h-40 bg-slate-400'></div>
-                        </div>
-                    </div>
 
-                    <button
-                        type="submit"
-                        disabled={isSubmitting}
-                        className="w-full h-14 mt-4 bg-plt-cream text-plt-dark transition-colors duration-500 hover:bg-plt-darkcream"
-                    >
-                        aceptar
-                    </button>
-                </Form>
-                </>
+                        <div className="w-full mt-5">
+                            <div className='flex items-center gap-2'>
+                                Imagenes adicionales
+                                <label htmlFor='extraImgs' className='text-slate-400 bg-slate-200 w-7 h-7 flex items-center justify-center cursor-pointer transition-colors duration-200 hover:bg-slate-300'>
+                                    <FaPlus size={15} />
+                                </label>
+                                <input type="file" id="extraImgs" className='hidden' />
+                            </div>
+                            <hr className='mt-2' />
+                            <div className='mt-3'>
+                                <div className='w-40 h-40 bg-slate-400'></div>
+                            </div>
+                        </div>
+
+                        <button
+                            type="submit"
+                            disabled={isSubmitting}
+                            className="w-full h-14 mt-4 bg-plt-cream text-plt-dark transition-colors duration-500 hover:bg-plt-darkcream"
+                        >
+                            aceptar
+                        </button>
+                    </Form>
+                </div>
             )}
         </Formik>
     )
