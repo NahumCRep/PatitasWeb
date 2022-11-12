4efe5201c6876b5a6d8f9de9fd2e957a32b1e8e4
Nahum Casco <nahumcasco.v@gmail.com>
2022-11-11 20:21:59 -0500

    new style of pet card

--- src/components/pets/PetCard.jsx
+++ src/components/pets/PetCard.jsx
@@ -2,46 +2,40 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 // ** Icons
 import { TbGenderMale, TbGenderFemale } from 'react-icons/tb';
+import {AiOutlineHeart, AiFillHeart} from 'react-icons/ai';
 import { BsBookmarkHeartFill } from 'react-icons/bs';
 import dogcard from '../../assets/images/dogcard.jpg'
 
 
-export const PetCard = ({pet}) => {
-  const baseUrl = pet.pet === 'perro' ? 'perros':'gatos';
+export const PetCard = ({ pet }) => {
+  const baseUrl = pet.pet === 'perro' ? 'perros' : 'gatos';
 
   return (
-    <div className={`h-60 relative font-secondary font-semibold bg-white rounded-xl overflow-hidden transition-colors duration-500
-      before:content-[''] before:w-full before:h-1/3 before:absolute before:top-0 before:left-0 before:z-0 
-      ${pet.pet === 'perro' ? ' border-2 before:bg-plt-blue hover:border-plt-blue':'border-2 before:bg-plt-cream hover:border-plt-cream'}`}>
-      
-      <div className="w-full h-full px-2 py-1 relative z-10">
-        <figure className="w-full h-2/3 ">
-          <img src={dogcard} alt="dog" className="w-full h-full rounded-xl object-contain" />
-        </figure>
+    <div className={`h-80 relative font-secondary font-semibold bg-white shadow-lg rounded-xl overflow-hidden transition-colors duration-500`}>
 
-        <div className="relative w-full h-full">
-            <button className="absolute top-0 right-0 text-slate-400">
-              <BsBookmarkHeartFill size={25} />
-            </button>
-            <p className="flex items-center gap-2">
-                <Link to={`/${baseUrl}/informacion/${pet.id}`}>{pet.name}</Link>
-                {/* <p onClick={petDetails}>{pet.name}</p> */}
-                {
-                    pet.genre === 'macho'
-                        ? <TbGenderMale color='#185ADB' />
-                        : <TbGenderFemale color='#E848F3' />
-                }
-            </p>
+      <figure className="w-full h-[82%] ">
+        <img src={dogcard} alt="dog" className="w-full h-full rounded-xl object-cover" />
+      </figure>
 
-            <div className="font-medium text-[0.7rem] text-slate-400">
-              <p className="w-full text-ellipsis">{pet.age}</p>
-              <p className="w-full text-ellipsis">{pet.breed}</p>
-              <p className="w-full text-ellipsis truncate">{pet.location}</p>
-            </div>
-
+      <div className="w-full h-[18%] px-4 flex items-center justify-between">
+        <div className="w-[80%]">
+          <p className="flex items-center gap-2 text-lg">
+            <Link to={`/${baseUrl}/informacion/${pet.id}`}>{pet.name}</Link>
+            {
+              pet.genre === 'macho'
+                ? <TbGenderMale color='#185ADB' />
+                : <TbGenderFemale color='#E848F3' />
+            }
+          </p>
+          <p className="w-full font-medium text-[0.8rem] text-slate-400 text-ellipsis truncate">
+            {pet.location}
+          </p>
         </div>
+
+        <button title={'agregar a favoritos'} className=" text-slate-400 transitio duration-200 hover:text-slate-500">
+          <AiFillHeart size={27} />
+        </button>
       </div>
-        
     </div>
   )
 }
