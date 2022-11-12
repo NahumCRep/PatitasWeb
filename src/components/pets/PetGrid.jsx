52fe8aea694571dc3e66f69398486bff59e41621
Nahum Casco <nahumcasco.v@gmail.com>
2022-11-11 20:22:50 -0500

    set 3 cols for md screen

--- src/components/pets/PetGrid.jsx
+++ src/components/pets/PetGrid.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const PetGrid = ({children}) => {
   return (
-    <div className="grid grid-cols-2 gap-3 p-5 md:grid-cols-4">
+    <div className="grid grid-cols-2 gap-5 p-5 md:grid-cols-3">
         {children}
     </div>
   )
