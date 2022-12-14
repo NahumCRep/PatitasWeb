f086526e62e3b5ce47605d66ab03ccd09f625fbd
Nahum Casco <nahumcasco.v@gmail.com>
2022-12-13 19:20:38 -0500

    add new icons

--- src/utils/reactIcons.js
+++ src/utils/reactIcons.js
@@ -1,12 +1,16 @@
+import { FaPlus, FaTimes } from 'react-icons/fa';
+import { MdEmail, MdLocationOn } from 'react-icons/md'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs'
-import { MdEmail } from 'react-icons/md'
+import { TbGenderMale, TbGenderFemale } from 'react-icons/tb';
 import { RiWhatsappFill } from 'react-icons/ri'
-import { FaPlus } from 'react-icons/fa';
 
 
 export { 
     BsFillInfoCircleFill, 
-    MdEmail, 
+    MdEmail, MdLocationOn, 
     RiWhatsappFill,
-    FaPlus 
+    FaPlus, FaTimes,
+    AiOutlineHeart, AiFillHeart,
+    TbGenderMale, TbGenderFemale 
 }
