463d6992ca2a9400ddfc3d4ccec8b049f2d2e15c
Nahum Casco <nahumcasco.v@gmail.com>
2023-01-26 22:00:13 -0500

    little change on style

--- src/components/profile/PublicationCard.jsx
+++ src/components/profile/PublicationCard.jsx
@@ -3,15 +3,23 @@ import { Link } from 'react-router-dom'
 
 export const PublicationCard = ({publication}) => {
   return (
-    <div className='bg-slate-100 p-1 shadow-md'>
+    <div className='bg-slate-100 p-1 shadow-md border-[1px]'>
+      <Link to={`/perfil/publicaciones/${publication._id}`}>
         <figure className='h-[80%] overflow-hidden'>
             <img 
                 src={publication.image}
                 alt={`foto de ${publication.name}`} 
-                className="h-full object-cover hover:scale-110 duration-200"
+                className="w-full h-full object-cover hover:scale-110 duration-200"
             />
         </figure>
-        <p className='mt-2 pl-1 font-primary text-plt-dark text-center text-sm'>{publication.name}</p>
+        <div className='mt-2 pl-1 font-secondary'>
+            <p className={`text-plt-dark text-left text-sm font-semibold border-b-2
+              ${publication.pet_type === 'dog' ? 'border-plt-blue ' : 'border-plt-cream'}`
+            }>
+              {publication.name}
+            </p>
+        </div>
+      </Link>
     </div>
   )
 }
