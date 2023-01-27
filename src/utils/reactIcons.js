41e5f644bfb77d1921db604581967cfc32247b23
Nahum Casco <nahumcasco.v@gmail.com>
2023-01-26 21:59:59 -0500

    new icons

--- src/utils/reactIcons.js
+++ src/utils/reactIcons.js
@@ -1,9 +1,10 @@
 import { FaPlus, FaTimes, FaUserCircle, FaDoorOpen } from 'react-icons/fa';
-import { MdEmail, MdLocationOn } from 'react-icons/md'
+import { MdEmail, MdLocationOn } from 'react-icons/md';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { BsFillInfoCircleFill, BsThreeDotsVertical } from 'react-icons/bs'
 import { TbGenderMale, TbGenderFemale } from 'react-icons/tb';
-import { RiWhatsappFill } from 'react-icons/ri'
+import { RiWhatsappFill } from 'react-icons/ri';
+import { TiWarning } from 'react-icons/ti';
 
 
 export { 
@@ -12,5 +13,6 @@ export {
     FaPlus, FaTimes, FaUserCircle, FaDoorOpen,
     MdEmail, MdLocationOn, 
     RiWhatsappFill,
-    TbGenderMale, TbGenderFemale 
+    TbGenderMale, TbGenderFemale,
+    TiWarning 
 }
