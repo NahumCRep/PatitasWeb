c3bf4af35b3b49d4e4cce305f26923bd85406a01
Nahum Casco <nahumcasco.v@gmail.com>
2023-02-05 23:16:28 -0500

    set height minimun as screen

--- src/components/pets/PetGrid.jsx
+++ src/components/pets/PetGrid.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const PetGrid = ({children}) => {
   return (
-    <div className="grid grid-cols-2 gap-5 p-5 md:grid-cols-4">
+    <div className="min-h-screen w-full grid grid-cols-2 gap-5 p-5 md:grid-cols-4">
         {children}
     </div>
   )
