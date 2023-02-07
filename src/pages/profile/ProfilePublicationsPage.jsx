04187ad480a6c4291b3701c42bafd763a93734c6
Nahum Casco <nahumcasco.v@gmail.com>
2023-02-06 23:13:03 -0500

    change style of page, new add publication button

--- src/pages/profile/ProfilePublicationsPage.jsx
+++ src/pages/profile/ProfilePublicationsPage.jsx
@@ -5,7 +5,7 @@ import { usePublicationStore, useAuthStore } from '../../hooks';
 import { ProfileLayout } from '../../components/layouts';
 import { PublicationCard } from '../../components/profile';
 // icons
-import { BsFillPlusCircleFill } from 'react-icons/bs';
+import { FaPlus } from '../../utils/reactIcons'
 
 export const ProfilePublicationsPage = () => {
   const navigate = useNavigate();
@@ -22,24 +22,28 @@ export const ProfilePublicationsPage = () => {
 
   return (
     <ProfileLayout layoutTitle={'Publicaciones'}>
-        <div className="w-full h-9 flex items-center">
+        <div className="w-full h-9 mb-2 flex items-center justify-between">
           <h2 className="font-secondary">Mis Publicaciones</h2>
-        </div>
-        <div className="mt-3 grid grid-cols-2  md:grid-cols-100x300 lg:grid-cols-5 gap-2">
           <button
             onClick={() => redirectToCreatePublicationPage()} 
-            className="flex items-center justify-center cursor-pointer 
-            bg-slate-100 text-slate-400 transition-colors 
-            duration-200  hover:text-plt-darkcream">
-            <BsFillPlusCircleFill size={40} />
+            className='flex items-center gap-1 rounded-md border-2
+             border-slate-200 py-1 px-2 transition-colors duration-300
+             hover:bg-slate-200 text-plt-dark'
+          >
+            <FaPlus />
+            <p className='hidden md:block'>
+              nueva publicacion
+            </p>
           </button>
+        </div>
+        <hr />
+        <div className="mt-3 grid grid-cols-2  md:grid-cols-100x300 lg:grid-cols-5 gap-2">
           {
-            publications 
-            && (
-              publications.map((publication) => (
+            publications && publications.length > 0
+            ? (publications.map((publication) => (
                 <PublicationCard key={publication._id} publication={publication} />
-              ))
-            )
+              )))
+            : <p className='text-slate-400'>No hay publicaciones</p>
           }
         </div>
     </ProfileLayout>
