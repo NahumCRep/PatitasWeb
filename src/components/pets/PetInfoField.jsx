03b904b3c8e70d358e8856cc9f07e39a8f9f064b
Nahum Casco <nahumcasco.v@gmail.com>
2023-02-07 22:08:10 -0500

    some css fixes

--- src/components/pets/PetInfoField.jsx
+++ src/components/pets/PetInfoField.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const PetInfoField = ({field, value}) => {
   return (
-    <div className="text-lg md:text-sm grid grid-cols-3">
+    <div className="text-lg md:text-xl flex gap-1">
         <p className="text-slate-400">
             {field} 
         </p>
