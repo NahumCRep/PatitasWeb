cf4dde28bbd384611f3564c456a3c355e88b4671
Nahum Casco <nahumcasco.v@gmail.com>
2023-02-11 22:09:26 -0500

    footer css and copyrigth message

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -9,8 +9,10 @@ export const Layout = ({ children }) => {
             <div className="min-h-screen pt-[80px] md:pt-[60px] bg-white">
                 {children}
             </div>
-            <footer className="w-full h-80 bg-plt-blue">
-
+            <footer className="w-full h-24 bg-plt-blue flex items-end justify-center p-2 font-secondary">
+                <p className='text-sm text-white'>
+                    &copy; patitas web todos los derechos reservados | {new Date().getFullYear()}
+                </p>
             </footer>
         </main>
     )
